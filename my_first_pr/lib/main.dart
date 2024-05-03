45e654c01dc0d8e556164f16eaaddbb5b3601729
MayurRajput97 <mayurrajput7827@gmail.com>
2024-05-04 00:18:20 +0530

    Network Images & Assets

--- my_first_pr/lib/main.dart
+++ my_first_pr/lib/main.dart
@@ -15,16 +15,14 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: const Center(
-        child: Text(
-          'Hello, I am Mayur Rajput',
-          style: TextStyle(
-            fontSize: 30.0,
-            letterSpacing: 3.0,
-            fontFamily: AutofillHints.countryName,
-            color: Colors.black,
-          ),
-        ),
+      // body: const Center(
+      //   child: Image(
+      //     image: NetworkImage(
+      //         'https://images.unsplash.com/photo-1595147389795-37094173bfd8?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+      //   ),
+      // ),
+      body: Center(
+        child: Image.asset('assets/giyu.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
