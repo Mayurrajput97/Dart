d9a119292fc05fb76b800d2681e10ced5affcdf3
MayurRajput97 <mayurrajput7827@gmail.com>
2024-05-04 10:28:54 +0530

    Icons

--- my_first_pr/lib/main.dart
+++ my_first_pr/lib/main.dart
@@ -21,9 +21,15 @@ class HomePage extends StatelessWidget {
       //         'https://images.unsplash.com/photo-1595147389795-37094173bfd8?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
       //   ),
       // ),
+      // body: Center(
+      //   child: Image.asset('assets/giyu.jpg'),
+      // ),
       body: Center(
-        child: Image.asset('assets/giyu.jpg'),
-      ),
+          child: Icon(
+        Icons.facebook,
+        color: Colors.blue,
+        size: 50.0,
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Text('Click It!'),
