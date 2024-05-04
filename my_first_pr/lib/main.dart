6fc53f8cde8a76e7c88ed2b5b45fb2a2ea725036
MayurRajput97 <mayurrajput7827@gmail.com>
2024-05-04 11:07:54 +0530

    Buttons

--- my_first_pr/lib/main.dart
+++ my_first_pr/lib/main.dart
@@ -15,21 +15,44 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
+
+      // NetWork Image :>>>>>>>>>>>>>>>>>>>>
+
       // body: const Center(
       //   child: Image(
       //     image: NetworkImage(
       //         'https://images.unsplash.com/photo-1595147389795-37094173bfd8?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
       //   ),
       // ),
+
+      // Assets Image :>>>>>>>>>>>>>>>>>>>>
+
       // body: Center(
       //   child: Image.asset('assets/giyu.jpg'),
       // ),
+
+      // Icons :>>>>>>>>>>>>>>>
+
+      // body: Center(
+      //     child: Icon(
+      //   Icons.facebook,
+      //   color: Colors.blue,
+      //   size: 50.0,
+      // )),
+
+      // Buttons  :>>>>>>>>>>>>>>>>>>>>
+
       body: Center(
-          child: Icon(
-        Icons.facebook,
-        color: Colors.blue,
-        size: 50.0,
+          child: ElevatedButton(
+        onPressed: () {
+          print('You Clicked me ');
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+        ),
+        child: Text('Its Centra!'),
       )),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Text('Click It!'),
