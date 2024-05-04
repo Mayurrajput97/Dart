ad51c349d9c2ddc2d17f8bdc0fe3c37ad642aa1f
MayurRajput97 <mayurrajput7827@gmail.com>
2024-05-04 23:59:30 +0530

    Column Widget

--- my_first_pr/lib/main.dart
+++ my_first_pr/lib/main.dart
@@ -10,45 +10,34 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome To Centra!'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
+        appBar: AppBar(
+          title: const Text('Welcome To Centra!'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
 
-      // Container & Padding :>>>>>>>>>>
+        //  Rows Widget :>>>>>>>>>>
 
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text('hello, world'),
-          TextButton(
-            onPressed: () {},
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.amber),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text('Container one'),
             ),
-            child: Text('click me'),
-          ),
-          Container(
-            color: Colors.cyan,
-            padding: EdgeInsets.all(30.0),
-            // margin: EdgeInsets.all(60.0),
-            child: Text('Iam in container'),
-          ),
-        ],
-      ),
-
-      // Padding Widget :>>>>>>>>>>>>>
-      // body: Padding(
-      //   padding: EdgeInsets.all(40.0),
-      //   child: Text('Hello Centra!'),
-      // )
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-      //   child: const Text('Click It!'),
-      //   backgroundColor: Color.fromARGB(255, 242, 79, 9),
-      // ),
-    );
+            Container(
+              padding: EdgeInsets.all(40.0),
+              color: Color.fromARGB(255, 233, 32, 9),
+              child: Text('Container Two'),
+            ),
+            Container(
+              padding: EdgeInsets.all(50.0),
+              color: Color.fromARGB(255, 165, 5, 239),
+              child: Text('Container Three'),
+            ),
+          ],
+        ));
   }
 }
