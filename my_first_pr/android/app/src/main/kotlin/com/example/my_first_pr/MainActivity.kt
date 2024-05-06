820d049ab5a26e45076e43cc9c029155747e9389
MayurRajput97 <mayurrajput7827@gmail.com>
2024-05-06 22:54:04 +0530

    Deletions

--- my_first_pr/android/app/src/main/kotlin/com/example/my_first_pr/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.my_first_pr
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ninja_id_project/android/app/src/main/kotlin/com/example/ninja_id_project/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ninja_id_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
