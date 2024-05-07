f273263120d417ee801f92daeb2ad8db7c18c22c
MayurRajput97 <mayurrajput7827@gmail.com>
2024-05-07 23:34:15 +0530

    final Commit

--- login_page/lib/main.dart
+++ login_page/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,45 +11,85 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Image.asset("Images/Insta.jpg", width: 90),
-          SizedBox(
-            height: 50,
-            child: TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.grey,
-                hintText: 'Phone Number',
-                hintStyle: TextStyle(fontSize: 20),
-                contentPadding: EdgeInsets.all(8.0),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black)),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
+          backgroundColor: Colors.white,
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 100, 30, 20),
+            child: Column(
+              children: [
+                Image.asset("Images/Insta.jpg", width: 90),
+                Padding(padding: EdgeInsets.only(top: 30)),
+                SizedBox(
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey,
+                      hintText: 'Phone Number OR Username',
+                      hintStyle: TextStyle(fontSize: 18),
+                      contentPadding: EdgeInsets.all(8.0),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey,
+                      hintText: 'Password ',
+                      hintStyle: TextStyle(fontSize: 18),
+                      contentPadding: EdgeInsets.all(8.0),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Forgot Pass?'),
+                  ),
+                ),
+                SizedBox(height: 40),
+                SizedBox(
+                  width: 200,
+                  height: 40,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'images/fb.png',
+                      width: 50,
+                    ),
+                    SizedBox(width: 20),
+                    Text(
+                      'Continue as Mayur Rajput',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-          SizedBox(
-            height: 50,
-            child: TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.grey,
-                hintText: 'password ',
-                hintStyle: TextStyle(fontSize: 20),
-                contentPadding: EdgeInsets.all(8.0),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black)),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    ));
+        ));
   }
 }
