7f18bda41d8cde9562930c77d4b084385ba22f6f
MayurRajput97 <mayurrajput7827@gmail.com>
2024-05-07 19:10:03 +0530

    Adding TextField Widget For Input

--- login_page/lib/main.dart
+++ login_page/lib/main.dart
@@ -15,6 +15,19 @@ class MyApp extends StatelessWidget {
       body: Column(
         children: [
           Image.asset("Images/Insta.jpg", width: 90),
+          TextField(
+            decoration: InputDecoration(
+              fillColor: Colors.grey,
+              hintText: 'Phone Number',
+              hintStyle: TextStyle(fontSize: 30),
+              contentPadding: EdgeInsets.all(8.0),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black)),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+              ),
+            ),
+          )
         ],
       ),
     ));
